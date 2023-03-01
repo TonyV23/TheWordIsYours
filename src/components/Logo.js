d10c665344aece21cec02c9ceb0546e9e722f773
viper23 <htonnycarlos@gmail.com>
2023-03-01 14:25:27 +0200

    add logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="" alt="react-logo" />
+      <h3>The world is Yours</h3>
+    </div>
+  );
+};
+
+export default Logo;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation/>
-            <h1>Accueil</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>Accueil</h1>
+    </div>
+  );
 };
 
 export default Home;
