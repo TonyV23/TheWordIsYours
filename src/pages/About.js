0f0ddd2bf338ef0376be106e5b03f53d201798be
viper23 <htonnycarlos@gmail.com>
2023-03-01 09:44:10 +0200

    add about page

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse id dolorum quasi totam repellendus voluptas magni. Recusandae error velit consequatur atque impedit. Provident maiores labore debitis iste qui maxime quidem rerum nam sequi quos inventore consequuntur voluptas non, atque cum in, molestiae est ex soluta architecto laborum quas facere ea minus. Nobis molestiae excepturi eos veritatis eveniet! Corporis ut totam aliquam tenetur aut vitae, ipsam animi numquam labore voluptatem repudiandae dolore assumenda. Asperiores quam culpa officia impedit atque, cumque modi dignissimos totam maxime aperiam repellat deleniti minus a beatae itaque adipisci, ex eius ea excepturi ad, ratione ullam tempore odio. Veniam temporibus optio, suscipit, dolorem labore nulla explicabo assumenda alias error doloribus facilis excepturi reiciendis saepe molestias iusto! Vel dolor nihil quisquam modi. Molestias inventore ad quisquam qui eos officiis nulla natus facilis voluptas non vero, accusantium recusandae odio rem, voluptatem excepturi perspiciatis quae voluptatibus maxime consequuntur sunt minus praesentium doloremque officia? Fugiat aperiam nostrum, quia corporis, in quidem perspiciatis dolores sapiente, animi alias error magni. Corrupti nobis totam ducimus placeat voluptate quasi libero sed facere voluptates. Quis molestias magni perspiciatis illo animi autem. Corporis ipsa magni vel dignissimos, officia ut! Ut, aliquam deserunt. Iste cupiditate quos ex quae rerum quasi quaerat, fugiat amet sed eum exercitationem esse recusandae maiores, sunt quisquam libero enim. Ratione accusamus commodi amet cum. Sint unde illum maxime vitae fugiat nulla magni soluta corporis modi eum vero laborum, architecto velit delectus dolorum cum, possimus quasi a qui accusamus quas? Fuga a doloribus sint fugit aut consectetur cupiditate, possimus mollitia exercitationem earum deleniti tempora ipsam incidunt, eum eveniet perspiciatis similique! Assumenda earum explicabo vero labore dolorum repellendus, quo sed delectus minima alias qui maxime voluptatem, ullam adipisci suscipit quae error fuga, voluptatibus totam dolores nemo animi a eum. Deserunt dolor voluptates reprehenderit, voluptas soluta corporis vel, a iusto iure odio non culpa itaque sequi, possimus saepe recusandae omnis beatae! Numquam maxime deleniti commodi corrupti eum quisquam ipsam minus animi dolor nisi, dolores iure magni odio suscipit soluta. Maiores consectetur sint quia optio rerum delectus nostrum dicta, laboriosam quae fugiat sequi! Laborum eveniet, facilis ullam veritatis temporibus, perspiciatis voluptatibus dolorum deserunt cumque in aspernatur, illo voluptatum eligendi mollitia commodi odit? Quia, officia cumque accusantium ex a aut amet nobis aspernatur, quas saepe sed veritatis, provident ipsa reprehenderit commodi nisi repellendus cum. Totam sunt, asperiores cum velit a doloremque recusandae tempore voluptas. Eveniet dolore id laborum sapiente. Molestias.
+            </p>
+        </div>
+    );
+};
+
+export default About;
