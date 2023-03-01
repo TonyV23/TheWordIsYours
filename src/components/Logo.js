1082d9bb6753b5362d1f79c743ce70c06de073f2
viper23 <htonnycarlos@gmail.com>
2023-03-01 17:23:38 +0200

    add countries components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo">
-      <img src="" alt="react-logo" />
+      <img src="./logo192.png" alt="react-logo" />
       <h3>The world is Yours</h3>
     </div>
   );
